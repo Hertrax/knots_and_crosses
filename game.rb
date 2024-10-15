de99de4e355defbf9631a96154dac17907412136
Hertrax <4ssume1t@gmail.com>
2024-10-15 19:24:23 +0500

    game instructions

---
+++ game.rb
@@ -0,0 +1,9 @@
+class Game
+  puts "Welcome to tic-tac-toe or knots-and-croses! You'll  choose places
+  as coordinates"
+  puts "for each turn enter the coordinates" 
+  puts '0 0 | 0 1 | 0 2'
+  puts '1 1 | 1 2 | 1 3'
+  puts '2 1 | 2 2 | 2 3'
+  puts "\r\n"
+end
